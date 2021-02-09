ae6759f90211205ff2bcc1591bd4a99d47054b84
oscarporras02 <oscarrussi@outlook.com>
2021-02-09 10:22:23 -0500

    first commit

---
+++ src/global/start.java
@@ -0,0 +1,61 @@
+package global;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Random;
+
+
+
+public class start {
+	static BufferedReader reader = new BufferedReader( new InputStreamReader(System.in)); 
+	public static void main(String[] args) throws IOException  {
+		int follow=0;
+			do {
+				play_exam();
+				System.out.println("Do you wan't to play again?: 1) yes 2) no");	
+		        follow = Integer.parseInt(reader.readLine());
+			}while(follow==1);
+	}
+	
+	public static void play_exam() throws NumberFormatException, IOException {
+		System.out.println("Type how many questions:");	
+        int total = Integer.parseInt(reader.readLine());
+        int sols=0;
+        for(int i=0; i<total; i++) {
+        	if(question()) sols++;
+        }
+        System.out.println("Final score: "+sols+"/"+total);
+	}
+	
+	public static boolean question() throws IOException{
+        ArrayList<String> letters = new ArrayList<String>( 
+        List.of("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S"
+        		,"T","U","V","W","X","Y","Z"));
+        ArrayList<String> subList = new ArrayList<String>();
+        Random rand = new Random();
+        String word = "";
+        int sol=0;
+        for(int i =0; i<5; i++) {
+	        int current = rand.nextInt(letters.size());
+	        String n = letters.get(current);
+	        subList.add(n);
+	        letters.remove(current);
+        }
+        for(int j=0; j<50;j++) {
+	        int index = rand.nextInt(subList.size());
+	        if(index==0) sol++;
+	        word+=subList.get(index);
+	        if((j+1)%10==0) word+="\n";
+        }
+        System.out.println("Find how many "+subList.get(0)+"'s?:");
+        System.out.println(word);	
+        String lett = reader.readLine();
+        if(Integer.parseInt(lett)==sol) System.out.println("correct");
+        else System.out.println("incorrect, the solution was "+sol);
+        return Integer.parseInt(lett)==sol;
+	}
+}
